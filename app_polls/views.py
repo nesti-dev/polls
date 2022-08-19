57292fe090f0058ce41dab58d34199b2309ff792
Ernesto Alejandro Santana Pérez <ernestoa7723@icloud.com>
2022-08-19 12:20:48 -0400

    writing your first django app, part 3 write views that actually do something, part 3

--- app_polls/views.py
+++ app_polls/views.py
@@ -9,11 +9,8 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('app_polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'app_polls/index.html', context)
 
 
 def detail(request, question_id):
