0461e87966185d16daf8ad9b7dd566443813a07d
Ernesto Alejandro Santana Pérez <ernestoa7723@icloud.com>
2022-08-19 12:35:19 -0400

    writing your first django app, part 4 use generic views: less code is better

--- app_polls/views.py
+++ app_polls/views.py
@@ -1,26 +1,29 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
+from django.views import generic
 
 from .models import Question, Choice
 
 # Create your views here.
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'app_polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'app_polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'app_polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'app_polls/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'app_polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'app_polls/results.html'
 
 
 def vote(request, question_id):
