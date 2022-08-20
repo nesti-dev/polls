d43551139a51c246ebe51fefbcfa2ef0ca5db207
Ernesto Alejandro Santana Pérez <ernestoa7723@icloud.com>
2022-08-19 20:42:44 -0400

    writing your first django app, part 7 customize the admin form, part 1

--- app_polls/admin.py
+++ app_polls/admin.py
@@ -4,4 +4,9 @@ from .models import Question
 
 # Register your models here.
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
